6d0bc283d87dd38c8bbd91cae86b9a891fc3b96e
Mike Gran <spk121@yahoo.com>
2013-09-15 20:36:04 -0700

    Clean up actions and log

--- broker/log.h
+++ broker/log.h
@@ -1,32 +1,58 @@
-#ifndef LOG_H
-#define	LOG_H
+/*
+    log.h - simple logging
 
-extern int opt_loglevel;
+    Copyright 2013 Michael L. Gran <spk121@yahoo.com>
+
+    This file is part of Jozabad.
+
+    Jozabad is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Jozabad is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Jozabad.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
+#ifndef JOZA_LOG_H
+#define	JOZA_LOG_H
+
+#include <czmq.h>
+
+extern int g_log_level;
+
 #define ERR(...)                                  \
     do {                                          \
-        if (opt_loglevel >= 1)                    \
+        if (g_log_level >= 1)                       \
             zclock_log("Error: " __VA_ARGS__);    \
     } while(0)
 #define WARN(...)                                   \
     do {                                            \
-        if (opt_loglevel >= 2)                      \
+        if (g_log_level >= 2)                         \
             zclock_log("Warning: "  __VA_ARGS__);   \
     } while(0)
 #define INFO(...)                               \
     do {                                        \
-        if (opt_loglevel >= 3)                  \
+        if (g_log_level >= 3)                     \
             zclock_log("Info: "  __VA_ARGS__);  \
     } while(0)
 #define NOTE(...)                               \
     do {                                        \
-        if (opt_loglevel >= 4)                  \
+        if (g_log_level >= 4)                     \
             zclock_log("Note: "  __VA_ARGS__);  \
     } while(0)
 #define TRACE(...)                              \
     do {                                        \
-        if (opt_loglevel >= 5)                  \
+        if (g_log_level >= 5)                     \
             zclock_log("Trace: "  __VA_ARGS__); \
     } while(0)
 
-#endif	/* LOG_H */
+#endif
 
