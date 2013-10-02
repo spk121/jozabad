f5a72aff8e420dbd29ea649c23a02b2da01c9dad
Mike Gran <spk121@yahoo.com>
2013-10-02 08:02:46 -0700

    more work on echo demo

--- tests/demo_echo.c
+++ tests/demo_echo.c
@@ -25,9 +25,9 @@
 #include "iodir.h"
 #include "packet.h"
 #include "tput.h"
+#include "mylimits.h"
 
 #include "initialize.h"
-#include "call_setup.h"
 
 int main(int argc, char** argv)
 {
@@ -37,34 +37,79 @@ int main(int argc, char** argv)
     iodir_t dir = io_outgoing_calls_barred;
     zctx_t *ctx = NULL;
     void *sock = NULL;
-    tput_t thru;
-    packet_t packet;
-    uint16_t window;
-    int ret;
+    int enabled = 0, connected = 0;
+    unsigned xpr = 0, xps = 0, ypr = 0, yps = 0, window = 0;
+    joza_msg_t *msg;
 
     initialize (verbose, "echo", &ctx, &sock, broker, calling_address, dir);
     while (1) {
         // Act on connection request
 
-        switch (id) {
-        case DATA:
-            // If PR allows it, echo the message back to the caller.
-            // Update flow control params.
-            break;
-        case RR:
-            // update flow control
-            // enable echoing
-            break;
-        case RNR:
-            // update flow control
-            // disable echoing
-            break;
-        case RESET_REQUEST:
-            // accept reset, resetting flow control params
-            break;
-        case CLEAR_REQUEST:
-            // accept clear, shutting down
-            break;
+        if (verbose)
+            printf("%s: waiting for a call request message\n", calling_address);
+        msg = joza_msg_recv(sock);
+        if (joza_msg_id(msg) != JOZA_MSG_CALL_REQUEST)
+            continue;
+        joza_msg_dump(msg);
+        window = joza_msg_window(msg);
+        joza_msg_send(&msg, sock);
+        connected = 1;
+        xpr = 0;
+        xps = 0;
+        ypr = 0;
+        yps = 0;
+
+        while (connected) {
+            msg = joza_msg_recv(sock);
+            joza_msg_dump(msg);
+            switch (joza_msg_id(msg)) {
+            case JOZA_MSG_DATA:
+                // If PR allows it, echo the message back to the caller.
+                ypr = joza_msg_pr(msg);
+                yps = joza_msg_ps(msg);
+                xpr = yps;      /* Always allow new data */
+                if (enabled && xps < ypr + window) {
+                    // Echo Y's msg back to him
+                    joza_msg_send_data(sock,
+                                       joza_msg_q(msg),
+                                       xpr,
+                                       xps,
+                                       joza_msg_data(msg));
+                    xps ++;
+                    if (xps > WINDOW_MAX)
+                        xps = 0;
+                }
+                else {
+                    joza_msg_send_rr(sock, xpr);
+                }
+                break;
+            case JOZA_MSG_RR:
+                // update flow control
+                // enable echoing
+                enabled = 1;
+                ypr = joza_msg_pr(msg);
+                break;
+            case JOZA_MSG_RNR:
+                // update flow control
+                // disable echoing
+                enabled = 0;
+                ypr = joza_msg_pr(msg);
+                break;
+            case JOZA_MSG_RESET_REQUEST:
+                // accept reset, resetting flow control params
+                joza_msg_send_reset_confirmation(sock);
+                xpr = 0;
+                xps = 0;
+                ypr = 0;
+                yps = 0;
+                break;
+            case JOZA_MSG_CLEAR_REQUEST:
+                // accept clear, shutting down
+                joza_msg_send_clear_confirmation(sock);
+                connected = 0;
+                break;
+            }
+            joza_msg_destroy(&msg);
         }
     }
     return (0);
